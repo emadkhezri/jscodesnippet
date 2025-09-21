59a7b31ba7ddc808ca6087fbc0584c98ed37e652
Emad Khezri <emad.khezri@gmail.com>
2025-09-21 23:27:28 +1000

    Selected active background color picker when user click on it

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ codeInput.addEventListener("input", function () {
 // update the main-container background
 function changeBackground(event) {
     const mainContainer = document.getElementById("main-container");
-    mainContainer.className = event.target.classList[1]
+    var selectedGradient = event.target.classList[1];
+    mainContainer.className = selectedGradient;
+
+    const colorPicker = document.getElementById("color-picker");
+    const allBgColors = colorPicker.querySelectorAll(".bgcolor-selector")
+    for (var index = 0; index < allBgColors.length; index++) {
+        const element = allBgColors[index];
+        if (!element.classList.contains(selectedGradient)) {
+            element.classList.remove("active")
+        } else {
+            element.classList.add("active")
+        }
+    }
 }
