06073249297a2368d34957ebbad6311e2976e235
Emad Khezri <emad.khezri@gmail.com>
2025-09-21 22:55:56 +1000

    Updated code section font

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Configure the code highlighter
 var codeInput = document.getElementById("codeInput");
 var codePreview = document.getElementById("codePreview");
 codeInput.addEventListener("input", function () {
     codePreview.innerHTML = hljs.highlight(codeInput.value, { language: "javascript" }).value;
 });
 
+// update the main-container background
 function changeBackground(event) {
     const mainContainer = document.getElementById("main-container");
     mainContainer.className = event.target.classList[1]
